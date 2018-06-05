cb221d560df0c6fd84d45422b6a9b8fca62267de
Lawrence Onah <paplow01@gmail.com>
2018-06-05 10:51:37 +0100

    Minor refactoring

--- send_mail.php
+++ send_mail.php
@@ -21,9 +21,21 @@ require_once("./includes/init.php");
 
 // Instantiate the mail object
 if (isset($_FILES['email_to_list']['tmp_name']) && is_file($_FILES['email_to_list']['tmp_name']))
-    $mail = new SendMail($_POST['subject'], $_POST['message'], $_POST['from'], $_FILES['email_to_list']['tmp_name'], (bool)$_POST['send_to_group']);
+    $mail = new SendMail(
+        $_POST['subject'], // Subject
+        $_POST['message'], // Message
+        $_POST['from'], // From
+        $_FILES['email_to_list']['tmp_name'], // To (Mail list file)
+        (bool)$_POST['send_to_group'] // Send to group
+    );
 else
-    $mail = new SendMail($_POST['subject'], $_POST['message'], $_POST['from'], $_POST['to'], (bool)$_POST['send_to_group']);
+    $mail = new SendMail(
+        $_POST['subject'], // Subject
+        $_POST['message'], // Message
+        $_POST['from'], // From
+        $_POST['to'], // To
+        (bool)$_POST['send_to_group'] // Send to single email
+    );
 
 // Validate & Send the Mail
 $mail->validateInput()->send();
